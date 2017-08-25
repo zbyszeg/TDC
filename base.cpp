456e31ea866d45f516302ee34853ea93150245fb
Zbyszek <zbyszek.gora@gmail.com>
2017-08-25 14:27:34 +0200

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -1,5 +1,3 @@
-#define _WIN32_WINNT 0x500
-
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
@@ -9,8 +7,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    HWND hWnd = GetConsoleWindow();
-    ShowWindow(hWnd, SW_HIDE);
+    ShowWindow(GetConsoleWindow(), 0);
 
     string name, line, command;
     fstream file;
