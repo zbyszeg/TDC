58d1005b0c75c572159f4a84174e6fce04da55ea
zbyszekgora <zbyszek.gora@gmail.com>
2017-08-26 14:07:24 +0200

    last changes

--- src/TDC.java
+++ src/TDC.java
@@ -13,11 +13,30 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import org.apache.commons.lang3.StringUtils;
+
+class Okno extends JFrame
+{
+	JProgressBar progressBar;
+	
+	public Okno()
+	{
+		setTitle("Zmiana danych...");
+		setSize(500,75);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		progressBar = new JProgressBar();
+		progressBar.setMinimum(0);
+		progressBar.setMaximum(100);
+		progressBar.setStringPainted(true);
+		add(progressBar);
+	}
+}
 
 public class TDC
 {	
@@ -52,16 +71,26 @@ public class TDC
 		rows = 0;
 		sopo = 0;
 		
+<<<<<<< HEAD
+//		JProgressBar bar = new JProgressBar(0, 100);
+//		bar.setValue(0);
+//		bar.setStringPainted(true);
+		
+		Okno okno = new Okno();
+		okno.setVisible(true);
+=======
+>>>>>>> 9bec86ef9ef9c17c3b0551fa6cb2bf1318a11d7a
 		try {
 			
 			BufferedReader in = new BufferedReader(new FileReader(name));
 
 			List<String> list = new ArrayList<String>();
-				
+			
 			while((str = in.readLine()) != null)
 			{
 				list.add(str+"\n");
 				rows++;
+				okno.progressBar.setValue(rows);
 			}
 				
 			in.close();
@@ -83,10 +112,22 @@ public class TDC
 			
 			save.print(tempFileBody);
 			save.close();
+<<<<<<< HEAD
+=======
 			JOptionPane.showMessageDialog(null, "Odczytane wiersze:        "+rows+"\nZmienione segmenty:   "+(sopo-1), "Zakończono pomyślnie", JOptionPane.INFORMATION_MESSAGE);
+>>>>>>> 9bec86ef9ef9c17c3b0551fa6cb2bf1318a11d7a
 			
+			JOptionPane.showMessageDialog(null, "Odczytane wiersze: "+rows+"\nZmienione segmenty: "+(sopo-1), "Zakończono pomyślnie", JOptionPane.INFORMATION_MESSAGE);
+		
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Nieprawidłowa nazwa pliku lub brak pliku w folderze. Spróbuj ponownie.", "Błąd!", JOptionPane.ERROR_MESSAGE);
 		}
+<<<<<<< HEAD
+		okno.dispose();
+		
+//		System.out.println("Odczytano wierszy:\t"+rows);
+//		System.out.println("Zmieniono pozycji:\t"+sopo);
+=======
+>>>>>>> 9bec86ef9ef9c17c3b0551fa6cb2bf1318a11d7a
 	}
 }
